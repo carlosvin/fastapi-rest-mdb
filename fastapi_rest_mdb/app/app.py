93a45782d8dbc30ffa3673e1ffaf656fca4a5dd4
Carlos Martin Sanchez <carlosvin@gmail.com>
2022-08-07 00:07:10 +0200

    make simpler the logger initialization

--- fastapi_rest_mdb/app/app.py
+++ fastapi_rest_mdb/app/app.py
@@ -1,12 +1,12 @@
 
 from importlib.metadata import version as get_version
-from logging import Logger, getLogger
+from logging import getLogger
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from fastapi_rest_mdb import api_v1
 from fastapi_rest_mdb.api_v1 import exception_handlers
 from fastapi_rest_mdb.api_v1.middlewares import register_middlewares
-from fastapi_rest_mdb.app.logger import config_logger, config_uvicorn_loggers, new_logger
+from fastapi_rest_mdb.app.logger import config_loggers
 
 from fastapi_rest_mdb.app.settings import Settings
 
@@ -20,8 +20,13 @@ class App:
         it initializes a fastAPI app
         """
         self._settings = settings
-        self._logger = new_logger(self.package(), settings.loglevel)
-        config_uvicorn_loggers(settings.loglevel)
+        self._logger = getLogger(self.package())
+        config_loggers(
+            settings.loglevel,
+            self.package(),
+            "uvicorn.access",
+            "uvicorn.error",
+            "fastapi")
 
         self._app = FastAPI(
             debug=settings.is_debug,
@@ -31,16 +36,6 @@ class App:
         self._app.include_router(api_v1.router)
         register_middlewares(self._app)
         exception_handlers.register(self._app, self._logger)
-
-    def _init_logger(self) -> Logger:
-        logger = getLogger(self.package())
-        config_logger(logger, self._settings.loglevel)
-        logger.info("initialized!", extra={'props': {'yay': 'yes'}})
-        return logger
-
-
-
-    
 
     @property
     def app(self) -> FastAPI:
--- fastapi_rest_mdb/app/logger.py
+++ fastapi_rest_mdb/app/logger.py
@@ -1,7 +1,6 @@
 
 
 from logging import Logger, StreamHandler, getLogger
-from sys import prefix
 from pythonjsonlogger import jsonlogger
 
 
@@ -12,18 +11,14 @@ def config_logger(logger: Logger, level: str) -> Logger:
     else:
         log_handler = StreamHandler()
         logger.addHandler(log_handler)
-    formatter = jsonlogger.JsonFormatter("%(levelname)s %(module)s %(filename)s %(lineno)s %(message)s %(threadName)s", timestamp=True)
+    formatter = jsonlogger.JsonFormatter(
+        "%(levelname)s %(module)s %(filename)s %(lineno)s %(message)s %(threadName)s",
+        timestamp=True,
+        static_fields={'log': logger.name})
     log_handler.setFormatter(formatter)
     return logger
 
 
-def new_logger(package: str, level: str) -> Logger:
-    logger = getLogger(package)
-    config_logger(logger, level)
-    logger.info("initialized!", extra={"foo": "bar"})
-    return logger
-
-def config_uvicorn_loggers(level: str) -> None:
-    loggers = ["uvicorn.access", "uvicorn.error", "fastapi"]
-    for logger in loggers:
-        config_logger(getLogger(logger), level)
+def config_loggers(level: str, *logger_names) -> None:
+    for logger_name in logger_names:
+        config_logger(getLogger(logger_name), level)
